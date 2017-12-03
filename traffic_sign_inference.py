4d76c64c01ff754caa5d93e038e5e305ff3600a8
Ramsey Khalaf <ramseykhalaf@gmail.com>
2017-12-04 01:32:23 +0800

    adding environment.yml & image resize

--- traffic_sign_inference.py
+++ traffic_sign_inference.py
@@ -10,12 +10,10 @@ from scipy.misc import imread
 from caffe_classes import class_names
 from alexnet import AlexNet
 
-x = tf.placeholder(tf.float32, (None, 32, 32, 3))
-# TODO: Resize the images so they can be fed into AlexNet.
-# HINT: Use `tf.image.resize_images` to resize the images
-resized = ...
 
-assert resized is not Ellipsis, "resized needs to modify the placeholder image size to (227,227)"
+x = tf.placeholder(tf.float32, (None, 32, 32, 3))
+resized = tf.image.resize_images(x, (227, 227))
+
 probs = AlexNet(resized)
 
 init = tf.global_variables_initializer()
